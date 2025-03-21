299d6704f831736c2e70d4f786e96cdf3e80e3c2
Engy-Mohamed <e.mohamed1984eg@gmail.com>
2025-03-21 16:20:08 +0200

    add dashboard t

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,44 @@
-using McqTask.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using McqTask.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace McqTask.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ExamContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ExamContext context, UserManager<ApplicationUser> userManager)
         {
-            _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var totalExams = await _context.Exams.CountAsync();
+            var activeExams = await _context.Exams.CountAsync(e => e.IsActive);
+            var studentCount = (await _userManager.GetUsersInRoleAsync("examtaker")).Count;
+
+
+
+
+            var examData = await _context.Exams
+                .OrderByDescending(e => e.ExamTime)
+                .Take(5)
+                .Select(e => new { e.Name, e.ExamTime })
+                .ToListAsync();
+
+            ViewBag.TotalExams = totalExams;
+            ViewBag.ActiveExams = activeExams;
+            ViewBag.StudentCount = studentCount;
+            ViewBag.ExamData = examData;
+
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
